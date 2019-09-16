2ee7469f9fe200c957e76066958738f9fcb96a34
Arsen4ik <46644855+Arsen4ik@users.noreply.github.com>
2019-09-16 16:40:28 +0300

    Updates

---
+++ precache-manifest.3da81ae11dbebf92c754ae999668d89e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779bf1fae925220b979e77fb2b4c857d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd2cb95e31cf91d858b2",
+    "url": "/monsters-rolodex/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "bd2cb95e31cf91d858b2",
+    "url": "/monsters-rolodex/static/js/main.8636c79d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
